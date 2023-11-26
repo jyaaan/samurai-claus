b99b646a054eafb6c110e35364c45b2ced390eee
John Yamashiro <john.yamashiro@gmail.com>
2023-11-26 15:47:44 -0800

    adding seasonal preference table

--- server/model/__init__.py
+++ server/model/__init__.py
@@ -2,4 +2,5 @@ from .member import Member
 from .message_log import MessageLog
 from .sequence import Sequence
 from .message_queue import MessageQueue
+from .seasonal_preference import SeasonalPreference
 print('this is __init__ from server/model')
